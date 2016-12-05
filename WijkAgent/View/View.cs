4af255e36ed6d468af6dea9a415f149b78a496b8
Nick Kronenberg <nikro412956@gmail.com>
2016-12-05 10:51:48 +0100

    tags gefixed en maximale lengte naar een variabele gemaakt

--- WijkAgent/View/View.cs
+++ WijkAgent/View/View.cs
@@ -33,6 +33,9 @@ namespace WijkAgent
         //laats geklikte label
         private Label lastClickedLabel;
 
+        //maximale tag lengte
+        private int tagLengte = 14;
+
         //placeholders
         private string searchDistrict = "Zoek een wijk . . .";
         private string searchUser = "Zoek een gebruiker . . .";
@@ -588,10 +591,10 @@ namespace WijkAgent
 
             foreach (var tag in tags)
             {
-                if(tag.Key.Length > 15)
+                if(tag.Key.Length > tagLengte)
                 {
                     string splittedTag = "";
-                    var tagSplit = tag.Key.SplitInParts(15);
+                    var tagSplit = tag.Key.SplitInParts(tagLengte);
                     foreach(string split in tagSplit)
                     {
                         splittedTag += split + " ";
