84e328e9d815a719eacbdc8b33e382ef7252c43b
Michel <meneervaartjes@hotmail.com>
2016-12-06 11:10:50 +0100

    Voorvallen scherm + querys

--- WijkAgent/Model/SQLConnection.cs
+++ WijkAgent/Model/SQLConnection.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using System.Windows.Forms;
 
 namespace WijkAgent.Model
 {
@@ -38,6 +39,83 @@ namespace WijkAgent.Model
 
         public void select()
         {
+
+        }
+        #region haal alle categorieën op methode
+        public Dictionary<int,string> GetAllCategory()
+        {
+            Dictionary<int,string> category = new Dictionary<int,string>();
+            try
+            {
+                this.conn.Open();
+                string stm = "SELECT * FROM category";
+                MySqlCommand command = new MySqlCommand(stm, this.conn);
+                this.rdr = command.ExecuteReader();
+                while (rdr.Read())
+                {
+                    category.Add(Int32.Parse(rdr.GetString(0)), rdr.GetString(1));
+                }
+                this.conn.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error bericht: " + e.Message);
+            }
+
+
+            return category;
+        }
+        #endregion
+        #region haal alle vandaag getwitterde twitterberichten in een wijk op
+        public Dictionary<int, string> GetAllTwitterMessageFromDistrictToday(int _idDistrict)
+        {
+            Dictionary<int, string> twitterMessages = new Dictionary<int, string>();
+
+            //goede format die ook in de database staat
+            string startDate = DateTime.Today.ToString("yyyy-MM-dd HH:mm:ss");
+            string endDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+            try
+            {
+                this.conn.Open();
+                string stm = "SELECT idtwitter, user, message FROM twitter WHERE iddistrict = @idDistrict AND datetime between @startDate AND @endDate AND save = false";
+                MySqlCommand command = new MySqlCommand(stm, this.conn);
+                command.Parameters.AddWithValue("@idDistrict", _idDistrict);
+                command.Parameters.AddWithValue("@startDate", startDate);
+                command.Parameters.AddWithValue("@endDate", endDate);
+                this.rdr = command.ExecuteReader();
+                while (rdr.Read())
+                {
+                    twitterMessages.Add(Int32.Parse(rdr.GetString(0)), rdr.GetString(1) + "\n" + rdr.GetString(2));
+                }
+
+                this.conn.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error bericht: " + e.Message);
+            }
+
+            return twitterMessages;
+        }
+        #endregion
+        internal void updateTwitterMessageCategory(int _twitterId, string _category)
+        {
+            try
+            {
+                this.conn.Open();
+                string stm = "UPDATE twitter SET idcategory = (SELECT idcategory FROM category WHERE name = @category), save = 1 WHERE idtwitter = @idTwitter";
+                MySqlCommand command = new MySqlCommand(stm, this.conn);
+                command.Parameters.AddWithValue("@idTwitter", _twitterId);
+                command.Parameters.AddWithValue("@category", _category);
+                command.ExecuteNonQuery();
+
+                this.conn.Close();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Error: " + e.Message);
+            }
 
         }
     }
---
+++ WijkAgent/View/IncidentScreen.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WijkAgent
+{
+    partial class IncidentScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectAllCheck = new System.Windows.Forms.CheckBox();
+            this.categoryCombo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.twitterIncidentPanel = new System.Windows.Forms.Panel();
+            this.saveIncidentButton = new System.Windows.Forms.Button();
+            this.cancelIncidentButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // selectAllCheck
+            // 
+            this.selectAllCheck.AutoSize = true;
+            this.selectAllCheck.Location = new System.Drawing.Point(43, 45);
+            this.selectAllCheck.Name = "selectAllCheck";
+            this.selectAllCheck.Size = new System.Drawing.Size(102, 17);
+            this.selectAllCheck.TabIndex = 0;
+            this.selectAllCheck.Text = "Alles Selecteren";
+            this.selectAllCheck.UseVisualStyleBackColor = true;
+            // 
+            // categoryCombo
+            // 
+            this.categoryCombo.FormattingEnabled = true;
+            this.categoryCombo.Location = new System.Drawing.Point(345, 41);
+            this.categoryCombo.Name = "categoryCombo";
+            this.categoryCombo.Size = new System.Drawing.Size(121, 21);
+            this.categoryCombo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(284, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Categorie*:";
+            // 
+            // twitterIncidentPanel
+            // 
+            this.twitterIncidentPanel.Location = new System.Drawing.Point(12, 79);
+            this.twitterIncidentPanel.Name = "twitterIncidentPanel";
+            this.twitterIncidentPanel.Size = new System.Drawing.Size(513, 422);
+            this.twitterIncidentPanel.TabIndex = 3;
+            // 
+            // saveIncidentButton
+            // 
+            this.saveIncidentButton.Location = new System.Drawing.Point(450, 530);
+            this.saveIncidentButton.Name = "saveIncidentButton";
+            this.saveIncidentButton.Size = new System.Drawing.Size(75, 23);
+            this.saveIncidentButton.TabIndex = 4;
+            this.saveIncidentButton.Text = "Opslaan";
+            this.saveIncidentButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelIncidentButton
+            // 
+            this.cancelIncidentButton.Location = new System.Drawing.Point(358, 530);
+            this.cancelIncidentButton.Name = "cancelIncidentButton";
+            this.cancelIncidentButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelIncidentButton.TabIndex = 5;
+            this.cancelIncidentButton.Text = "Annuleren";
+            this.cancelIncidentButton.UseVisualStyleBackColor = true;
+            // 
+            // IncidentScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 565);
+            this.Controls.Add(this.cancelIncidentButton);
+            this.Controls.Add(this.saveIncidentButton);
+            this.Controls.Add(this.twitterIncidentPanel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.categoryCombo);
+            this.Controls.Add(this.selectAllCheck);
+            this.Name = "IncidentScreen";
+            this.Text = "IncidentScreen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox selectAllCheck;
+        private System.Windows.Forms.ComboBox categoryCombo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel twitterIncidentPanel;
+        private System.Windows.Forms.Button saveIncidentButton;
+        private System.Windows.Forms.Button cancelIncidentButton;
+    }
+}
