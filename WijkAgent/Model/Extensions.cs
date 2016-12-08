574ac655e5983fd73f4fef77bef868e833210680
Nick Kronenberg <nikro412956@gmail.com>
2016-12-08 13:34:48 +0100

    extension korter gemaakt en verbeterd

--- WijkAgent/Model/Extensions.cs
+++ WijkAgent/Model/Extensions.cs
@@ -8,13 +8,8 @@ namespace WijkAgent.Model
 {
     static class Extensions
     {
-        public static IEnumerable<String> SplitInParts(this String s, Int32 partLength)
+        public static IEnumerable<string> SplitInParts(this string s, int partLength)
         {
-            if (s == null)
-                throw new ArgumentNullException("s");
-            if (partLength <= 0)
-                throw new ArgumentException("Part length has to be positive.", "partLength");
-
             for (var i = 0; i < s.Length; i += partLength)
                 yield return s.Substring(i, Math.Min(partLength, s.Length - i));
         }
