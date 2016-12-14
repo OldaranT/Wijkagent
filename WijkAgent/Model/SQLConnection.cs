d140a3a2c5498c76859c18150864d177858f7353
timbaak@hotmail.com <timbaak@hotmail.com>
2016-12-14 15:12:03 +0100

    Fixed open close database

--- WijkAgent/Model/SQLConnection.cs
+++ WijkAgent/Model/SQLConnection.cs
@@ -346,9 +346,10 @@ namespace WijkAgent.Model
         public Dictionary<int, string> GetAllAdjacentDistricts(int _idDistrict)
         {
             Dictionary<int, string> adjecentDistricts = new Dictionary<int, string>();
+
+            conn.Open();
             try
             {
-                this.conn.Open();
                 string stmt = "SELECT district.iddistrict, district.name FROM district JOIN neighbordistrict ON neighbordistrict.idneighbordistrict = district.iddistrict WHERE neighbordistrict.iddistrict = @idDistrict";
                 MySqlCommand command = new MySqlCommand(stmt, this.conn);
                 command.Parameters.AddWithValue("@idDistrict", _idDistrict);
@@ -357,13 +358,13 @@ namespace WijkAgent.Model
                 {
                     adjecentDistricts.Add(Int32.Parse(rdr.GetString(0)), rdr.GetString(1));
                 }
-
-                this.conn.Close();
             }
             catch (Exception e)
             {
                 MessageBox.Show("Error bericht: " + e.Message);
             }
+
+            conn.Close();
 
             return adjecentDistricts;
         }
