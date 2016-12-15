0c720c51f0bb788976755660b6d9f011592af502
timbaak@hotmail.com <timbaak@hotmail.com>
2016-12-15 11:46:08 +0100

    Added label for area districts

--- WijkAgent/View/View.Designer.cs
+++ WijkAgent/View/View.Designer.cs
@@ -113,6 +113,7 @@
             this.history_till_datetimepicker = new System.Windows.Forms.DateTimePicker();
             this.history_keyword_checkbox = new System.Windows.Forms.CheckBox();
             this.history_search_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.main_menu_panel.SuspendLayout();
             this.main_menu_table_layout_panel.SuspendLayout();
             this.main_menu_panel_for_label.SuspendLayout();
@@ -254,6 +255,7 @@
             // main_menu_selected_district_panel
             // 
             this.main_menu_selected_district_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(70)))), ((int)(((byte)(130)))));
+            this.main_menu_selected_district_panel.Controls.Add(this.label1);
             this.main_menu_selected_district_panel.Controls.Add(this.main_menu_selected_district_label);
             this.main_menu_selected_district_panel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.main_menu_selected_district_panel.ForeColor = System.Drawing.SystemColors.ControlText;
@@ -1276,6 +1278,20 @@
             this.history_search_button.UseVisualStyleBackColor = false;
             this.history_search_button.Click += new System.EventHandler(this.history_search_button_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, -6);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Omliggende wijken:";
+            // 
             // View
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -1429,6 +1445,7 @@
         private System.Windows.Forms.Label twitter_taglabel2;
         private System.Windows.Forms.Label twitter_taglabel1;
         private System.Windows.Forms.Button clean_district_tweets_button;
+        private System.Windows.Forms.Label label1;
     }
 }
 
